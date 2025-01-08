0b5cdebc3232bb55890562027752ea25a02bcf09
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-01-08 16:29:15 +0300

    back stack entry...

--- app/src/main/kotlin/test/android/cns2/module/test/TestScreen.kt
+++ app/src/main/kotlin/test/android/cns2/module/test/TestScreen.kt
@@ -5,25 +5,33 @@ import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.createGraph
 import androidx.navigation.navArgument
 
 @Composable
@@ -51,10 +59,12 @@ private fun TestScreen(
 }
 
 @Composable
-internal fun TestScreen() {
-    val nhc = rememberNavController()
+internal fun TestScreen(
+    modifier: Modifier,
+    nhc: NavHostController,
+) {
     NavHost(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = modifier,
         navController = nhc,
         startDestination = "f1",
     ) {
@@ -102,5 +112,33 @@ internal fun TestScreen() {
                 },
             )
         }
+    }
+}
+
+@Composable
+internal fun TestScreen() {
+    val insets = WindowInsets.systemBars.asPaddingValues()
+    val nhc = rememberNavController()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(insets),
+    ) {
+        TestScreen(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            nhc = nhc,
+        )
+        val bse = nhc.currentBackStackEntryAsState().value
+        BasicText(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp)
+                .padding(start = 8.dp)
+                .wrapContentHeight(),
+            text = "route: ${bse?.destination?.route}",
+        )
     }
 }
